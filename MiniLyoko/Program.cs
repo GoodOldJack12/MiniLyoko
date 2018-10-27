fb1156437b2349557d180082140504983b051f4c
GoodOldJack12 <deloght.jonathan@gmail.com>
2018-10-27 19:15:22 +0200

    Initial commit

---
+++ MiniLyoko/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using LyokoAPI.API;
+using LyokoAPI.VirtualStructures;
+using LyokoAPI.VirtualStructures.Interfaces;
+
+namespace MiniLyoko
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ITower tower = new APITower("lyoko", "ice", 1, true);
+            tower.Activator = LyokoParser.ParseActivator("XANA");
+            Console.WriteLine(tower.Sector.Name);
+            Console.WriteLine(tower.Number);
+        }
+    }
+}
