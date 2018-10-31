b0966896e45e8013c33e61e12a733d660e845a5c
GoodOldJack12 <deloght.jonathan@gmail.com>
2018-10-31 15:14:22 +0100

    towers start counting from 1, idiot

--- MiniLyoko/VirtualStructures/Sector.cs
+++ MiniLyoko/VirtualStructures/Sector.cs
@@ -42,7 +42,7 @@ namespace MiniLyoko
 
         public void ActivateRandom(APIActivator activator = APIActivator.XANA)
         {
-            int randomint = new Random().Next(Towers.Count-1);
+            int randomint = new Random().Next(Towers.Count)+1;
             GetTower(randomint)?.Activate(activator);
         }
     }
